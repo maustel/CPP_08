5bf53d5d93c873bba29925f2020c00c51b3fb7b2
Milena Austel <maustel@2-D-7.42heilbronn.de>
2025-04-23 15:11:12 +0200

    ex00 done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,21 +6,40 @@
 /*   By: maustel <maustel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 12:13:13 by maustel           #+#    #+#             */
-/*   Updated: 2025/04/23 12:45:23 by maustel          ###   ########.fr       */
+/*   Updated: 2025/04/23 15:01:36 by maustel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __EASYFIND_HPP__
 # define __EASYFIND_HPP__
 
-# include <iostream>
-# include <array>
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
+#include <vector>
+#include <list>
+#include <array>
+#include <iostream>
 
+/*
+std::find(first, last, value)
+first: An iterator pointing to the start of the search range (container.begin())
+last: An iterator pointing to one position past the end of the search range (container.end())
+value: The element to search for
+
+Iterators are designed to abstract away the complexities of different container implementations
+while providing pointer-like functionality. They're not necessarily pointers themselves, but
+rather objects that implement pointer-like behavior specifically for traversing containers
+safely and efficiently.
+*/
 template <typename T>
-typename T::iterator easyfind(T& container, int index)
+typename T::iterator easyfind(T& container, int val)
 {
-	typename T::iterator i = std::find(container.begin(), container.end(), index);
-
+	typename T::iterator i = std::find(container.begin(), container.end(), val);
+	if (i == container.end())
+	{
+		throw std::runtime_error("Value not found in container");
+	}
 	return (i);
 }
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,15 +6,55 @@
 /*   By: maustel <maustel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 12:13:10 by maustel           #+#    #+#             */
-/*   Updated: 2025/04/23 12:25:39 by maustel          ###   ########.fr       */
+/*   Updated: 2025/04/23 15:10:11 by maustel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+/*
+The auto keyword lets the compiler deduce the correct iterator type based on the container being used.
+*/
+
+
 int main()
 {
-	std::cout << "Something\n";
+	std::vector<int> vec = {2, 4, 6, 8, 42};
+	std::list<int> lst = {2, 4, 6, 8, 42};
+	std::array<int, 5> arr = {2, 4, 6, 8, 42};
+
+	//-------------[VECTOR TEST]------------------------
+	try
+	{
+		auto iterator = easyfind(vec, 4);
+		std::cout << "Value found in vec at position: " << *iterator << " with address: " << &(*iterator) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << " vec" << '\n';
+	}
+
+	//-------------[LIST TEST]------------------------
+	try
+	{
+		auto iterator = easyfind(lst, 8);
+		std::cout << "Value found in lst at position: " << *iterator << " with address: " << &(*iterator) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << " lst" << '\n';
+	}
+
+	//-------------[ARRAY TEST]------------------------
+	try
+	{
+		auto iterator = easyfind(arr, 42);
+		std::cout << "Value found in arr at position: " << *iterator << " with address: " << &(*iterator) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << " arr" << '\n';
+	}
 
 	return (0);
 }
